77aa839d01f96f786aca2ca31233741ccca2cc81
anis.khalfaoui <anis.khalfaoui@proxym-it.com>
2022-04-02 22:02:15 +0100

    add api endpoints

--- vue/src/util.js
+++ vue/src/util.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export function login(credentials) {
     return new Promise((res, rej) => {
-        axios.post('http://localhost:5000/api/auth/signup', credentials)
+        axios.post('http://localhost:5000/api/auth/signin', credentials)
             .then((response) => {
                // setAuthorization(response.data.access_token);
                 res(response.data);
